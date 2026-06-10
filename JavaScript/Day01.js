4aa27d078bb2b5dbe33a0965609cf8062fb11619
Vidyasagar Yadav <146695818+vidyasagar0007@users.noreply.github.com>
2026-06-10 12:06:22 +0530

    This is my JsProblem file

--- JavaScript/Day01.js
+++ JavaScript/Day01.js
@@ -5,15 +5,48 @@
 
 // Question here:--------------
 
-a = 10;
-b = 20;
-c = 30;
-d = 40;
-x = a + b;
-y = c + d;
+// a = 10;
+// b = 20;
+// c = 30;
+// d = 40;
+// x = a + b;
+// y = c + d;
+
 // this is my first question (Add x+y both are the True)
-console.log((a + b && c + d) >= 50)
-// this is my second question(At least one true) 
-console.log((x>=10)||(y>=100))
+// console.log((a + b && c + d) >= 50)
+// this is my second question(At least one true)
+// console.log((x>=10)||(y>=100))
 // this is my third question here (Less then 50)
-console.log((x<=50)&&(y<50))
+// console.log((x<=50)&&(y<50))
+
+//if and ifelse condition here
+
+// const a=80
+
+// if (a>=90) {
+//     console.log("A Grade")
+// }
+
+// else if(a>=75) {
+
+//     console.log("B Grade")
+// }
+// else if(a>=60){
+//     console.log(" C Grade")
+
+//     } else {
+//         console.log("fail")
+
+//     }
+
+//=======================//Admin|| HR || User  Role here ==========================
+
+const role = "Admin";
+
+if (role === "Admin") {
+  console.log(" Welcome " + role);
+} else if (role === "HR") {
+  console.log(" Welcome " + role);
+} else {
+  console.log("Please Register");
+}
---
+++ JavaScript/JavaProblem.js
@@ -0,0 +1,36 @@
+const product ="4"
+
+if (product===1) {
+    console.log("Swift "+ ""+ product)
+}
+else if (product===2) {
+    console.log("BMW " + " " + product)
+}
+else if (product===3) {
+    console.log("hero car " + " " + product)
+}
+else if (product===4) {
+    console.log("KN Car " + " " + product)
+}
+else if (product===5) {
+    console.log("cyber Track " + " " + product)
+}
+else if (product==="1") {
+    console.log("apple "  + product)
+}
+else if (product==="2") {
+    console.log("Banana " + product)
+}
+else if (product==="3") {
+    console.log("Orange "+ product)
+}
+else if (product==="4") {
+    console.log("Lichi " + product)
+}
+else if (product==="5") {
+    console.log("Pineapple " + product)
+} else{
+    console.log("Product is not find")
+}
+
+
