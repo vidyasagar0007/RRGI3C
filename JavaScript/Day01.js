2609b143cc0e05796ea2375d8051af5a1b9a0d42
Vidyasagar Yadav <146695818+vidyasagar0007@users.noreply.github.com>
2026-06-10 11:15:26 +0530

    This is my js file day01

---
+++ JavaScript/Day01.js
@@ -0,0 +1,19 @@
+// a=10
+// console.log(a)
+// const a="bahratr"
+// console.log(typeof(a))
+
+// Question here:--------------
+
+a = 10;
+b = 20;
+c = 30;
+d = 40;
+x = a + b;
+y = c + d;
+// this is my first question (Add x+y both are the True)
+console.log((a + b && c + d) >= 50)
+// this is my second question(At least one true) 
+console.log((x>=10)||(y>=100))
+// this is my third question here (Less then 50)
+console.log((x<=50)&&(y<50))
